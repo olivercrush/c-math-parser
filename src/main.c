d5edda75b20563c980c4f0cdd45b310c89b987d6
olivercrush <olivier.beguinti@gmail.com>
2026-08-29 20:01:18 -0400

    added tokenize method

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum {
     EXPRESSION,
@@ -23,6 +24,11 @@ const char OPERATION_TOKENS[] = {
     [DIVIDE] = '/'
 };
 
+typedef struct Tokenized {
+    int count;
+    char **tokens;
+} Tokenized;
+
 typedef struct Node {
     int type;
     int data;
@@ -31,6 +37,7 @@ typedef struct Node {
 } Node;
 
 int get_operation_token(char token);
+Tokenized tokenize_string(char *expression, int length);
 Node* create_node(int type);
 Node* build_parse_tree(char *expression, int length);
 void print_node(Node* root);
@@ -38,11 +45,12 @@ void free_node(Node* root);
 
 int main() {
     char expression[] = "1+2*3-5";
+    Tokenized tokenized = tokenize_string(expression, 7);
 
-    Node* root = build_parse_tree(expression, 7);
-    print_node(root);
+    //Node* root = build_parse_tree(expression, 7);
+    //print_node(root);
 
-    free_node(root);
+    //free_node(root);
     return 0;
 }
 
@@ -51,6 +59,49 @@ int get_operation_token(char token) {
         if (OPERATION_TOKENS[i] == token)
             return i;
     return NO_OP;
+}
+
+Tokenized tokenize_string(char *expression, int length) {
+    int next_operation_cursor = 0;
+    int current_term_cursor = 0;
+
+    int max_token_count = length;
+    char *tokens[max_token_count];
+
+    int token_count = 0;
+    while (next_operation_cursor < length) {
+        // 1. Get next operation
+        do
+            next_operation_cursor++;
+        while (next_operation_cursor < length && get_operation_token(expression[next_operation_cursor]) == NO_OP);
+
+        // 2. Tokenize term
+        int term_length = next_operation_cursor - current_term_cursor;
+        if (term_length > 0)
+        {
+            tokens[token_count] = malloc(term_length + 1);
+            memcpy(tokens[token_count], &expression[current_term_cursor], term_length);
+            tokens[token_count][term_length] = '\0';
+            token_count++;
+        }
+        current_term_cursor = next_operation_cursor + 1;
+
+        // 3. Tokenize operation
+        if (next_operation_cursor < length) {
+            tokens[token_count] = malloc(2);
+            memcpy(tokens[token_count], &expression[next_operation_cursor], 1);
+            tokens[token_count][1] = '\0';
+            token_count++;
+        }
+    }
+
+    Tokenized tokenized = { token_count };
+    tokenized.tokens = malloc(token_count * sizeof(char*));
+
+    for (int i = 0; i < token_count; i++)
+        tokenized.tokens[i] = tokens[i];
+
+    return tokenized;
 }
 
 Node* create_node(int type) {
